432afee4907c8c4a7710c958a1f9d0d23dcf8570
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-03-01 23:35:23 +0600

    footer done

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,35 +1,36 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <footer className="footer bg-black text-white p-10 sm:flex sm:flex-col lg:flex-row lg:items-center lg:justify-evenly">
+        <footer className="footer bg-gray-600 border-t text-black p-10 sm:flex sm:flex-col lg:flex-row lg:items-center lg:justify-evenly">
             <div className='lg:w-1/3'>
-                <span className="footer-title headingM ">Paradise Palms</span>
-                <p className='mt-10 paragraph'>Our tour hostel is also equipped with all the amenities you need to make your stay comfortable. We have a fully equipped kitchen, a lounge area with comfortable seating, and a library where you can relax and read a book.</p>
+                <span className="font-bold headingM ">Paradise Palms</span>
+                <p className='mt-10 font-openSans'>Our tour hostel is also equipped with all the amenities you need to make your stay comfortable. We have a fully equipped kitchen, a lounge area with comfortable seating, and a library where you can relax and read a book.</p>
                 <div><FontAwesomeIcon ></FontAwesomeIcon></div>
             </div>
-                <div>
-                    <span className="footer-title">Quick Link</span>
-                    <a className="link link-hover">About Hotel</a>
-                    <a className="link link-hover">Terms & Conditionis</a>
-                    <a className="link link-hover">Jobs</a>
-                </div>
-                <div>
-                    <span className="footer-title">Contact Info</span>
-                    <a className="link link-hover">(1800) 574 9687</a>
-                    <a className="link link-hover">256, Resort Street,, New York 24</a>
-                    <a className="link link-hover">Email : info@paradisepalms.com</a>
-                </div>
             <div>
-                <span className="footer-title">Newsletter</span>
+                <span className="font-bold font-diplayFair">Quick Link</span>
+                <a className="link link-hover font-openSans">About Hotel</a>
+                <a className="link link-hover font-openSans">Terms & Conditionis</a>
+                <a className="link link-hover font-openSans">Jobs</a>
+            </div>
+            <div>
+                <span className="font-bold font-diplayFair">Contact Info</span>
+                <a className="link link-hover font-openSans">Phone: +88953 445 4892</a>
+                <a className="link link-hover font-openSans">256, Resort Street,, New York 24</a>
+                <a className="link link-hover font-openSans">Email: info@paradisepalms.com</a>
+            </div>
+            <div>
+                <span className="font-bold font-diplayFair">Newsletter</span>
                 <div className="form-control w-80">
                     <label className="label">
                         <span className="label-text">Enter your email address</span>
                     </label>
                     <div className="relative">
-                        <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
-                        <button className="Btn-Primary mt-6">Subscribe</button>
+                        <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16 mb-10" />
+                        <Link to="/services" className="Btn-Primary">Subscribe</Link>
                     </div>
                 </div>
             </div>
